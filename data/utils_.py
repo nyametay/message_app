922720880ccfac138337d009ee20effcf25e24d8
Isaac Nyame Taylor <isaac4230220@gmail.com>
2025-09-26 11:42:27 +0000

    Update utils_.py

--- data/utils_.py
+++ data/utils_.py
@@ -5,10 +5,11 @@ from data.utils.whatsapp_utils import send_whatsapp
 
 def send_message(channel, contact, content):
     if channel.lower() == 'email' and contact.email:
-        send_email_smtp(contact.email, content)
+        send_email(contact.email, content)
     elif channel.lower() == 'sms' and contact.phone:
         send_sms(contact.phone, content)
     elif channel.lower() == 'whatsapp' and contact.phone:
         send_whatsapp(contact.phone, content)
     else:
         raise ValueError(f"No valid contact method for channel: {channel}")
+
